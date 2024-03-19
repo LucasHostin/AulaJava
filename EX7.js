fe014b4ad820f1347babda5591f0c5ab21b5d75d
LucasHostin <148364351+LucasHostin@users.noreply.github.com>
2024-03-19 19:18:57 -0300

    Add files via upload

---
+++ EX7.js
@@ -0,0 +1,24 @@
+// EXE7
+
+// trazer os seguintes dados de 5 usuarios 
+
+// NOME 
+// SOBRENOME
+// CIDADE 
+// CPF 
+// NOME DA AGENCIA DE BANCO EX: NUBANK, SANTANDER...
+// SALARIO 
+// FAZER A SOMA DOS SALDOS SALARIAIS 
+// DEPOIS FAZER A DIVISAO POR 6
+// E FAZER UMA IF E ELSE 
+// SE A PESSSOA É MILHONARIA, RICA, BEM DE VIDA, CLASSE MEDIA E POBRE 
+// E ULTILIZAR O FOR PARA FICAR MAIS FACIL DE CODAR ESSA ATIVIDADE 
+// E FAZER UMA ARRAY
+// E FAZER UM FOR 
+
+
+// DEPOIS FAZER UMA SAUDACAO 
+// EX:  seu nome é lucas... sua idade.. seu cpf.. sua conta... 
+
+var nomes = ['maykon','Lucas', 'yuri', 'joao'];
+console.log(nomes[2]);
